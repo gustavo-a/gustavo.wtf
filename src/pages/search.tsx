822cd243d39cd9be099e23c7efd437a0c6e52f4e
Gustavo Rocha <gustavo@onserp.com.br>
2021-01-05 21:43:06 -0300

    search page and icon

---
+++ src/pages/search.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import Layout from '@components/layout'
+import SearchField from '@components/shared/search/searchField'
+
+import Arrow from '@svg/arrow.svg'
+
+const search: React.FC = () => {
+  return (
+    <Layout>
+      <div className="container">
+        <div className="flex justify-center py-16">
+          <div className="xl:w-4/5">
+            <div className="flex justify-between mb-8 items-center">
+              <h1 className="text-3xl font-display font-bold">Buscar</h1>
+              <button
+                aria-label="Voltar para a página anterior"
+                className="text-gray-700 dark:text-gray-300 flex items-center"
+                onClick={() => history.back()}
+              >
+                <Arrow className="transform-gpu rotate-180 w-4 ml-0 mr-2 fill-current" />
+                <span>Voltar</span>
+              </button>
+            </div>
+            <SearchField />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default search
