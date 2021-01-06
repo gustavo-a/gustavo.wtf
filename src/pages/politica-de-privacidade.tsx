87aaef78924eef12c4eaf2f0828148ad8303ff0b
Gustavo Rocha <gustavo@onserp.com.br>
2021-01-05 21:41:00 -0300

    add privacy policy page

---
+++ src/pages/politica-de-privacidade.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Layout from '@components/layout'
+
+const politicaDePrivacidade: React.FC = () => {
+  return <Layout></Layout>
+}
+
+export default politicaDePrivacidade
