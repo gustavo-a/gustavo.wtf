f5fa1f4401afd90ff553c45049c6a9283d2cce2b
Gustavo <gustavo_641@live.com>
2021-01-09 10:14:33 -0300

    edit 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,8 +6,17 @@ const NotFoundPage = () => (
   <div>
     <Layout>
       <Seo title="Página não encontrada | Gustavo Rocha" />
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <div className="flex justify-center items-center mt-32 max-w-2xl mx-auto px-4">
+        <div className="text-center">
+          <h1 className="text-5xl font-display font-bold mb-4">
+            404: Página não encontrada
+          </h1>
+          <p className="text-lg">
+            Parace que alguma coisa deu errado aí... Volte algumas casas e
+            aguarde novamente a sua vez de jogar.
+          </p>
+        </div>
+      </div>
     </Layout>
   </div>
 )
